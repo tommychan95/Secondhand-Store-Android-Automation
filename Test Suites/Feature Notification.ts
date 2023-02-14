1161712e9f495e7ff537fa51246737f45f214d0e
tommychan95-admin <tommy.chaniago42@gmail.com>
2023-02-15 00:24:14 +0700

    add notification test suites ts

---
+++ Test Suites/Feature Notification.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Notification</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c46c2f5f-2755-4bc3-85f2-08b01a2778fe</testSuiteGuid>
+   <testCaseLink>
+      <guid>fcc9e79f-06c9-42d4-9785-e87fb3c3cac8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Notification/NO001 - User want to view notification after login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
