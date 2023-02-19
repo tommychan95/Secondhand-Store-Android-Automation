69b0f9f7b6d6778f0acadcb242526ec4b2045a0b
Desyntha <ndesyntha@gmail.com>
2023-02-19 11:06:18 +0700

    menambahkan test suite delete produk ts

---
+++ Test Suites/Feature Delete Product.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Delete Product</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5f087f85-e666-4682-ab0a-a830b2a4e8d6</testSuiteGuid>
+   <testCaseLink>
+      <guid>4650402c-adc4-4a63-96f8-cdc1f6109fe5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Delete Product/DP001 - User tap delete button but does not delete the product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d3399fe5-f315-4ec1-84b6-c8e3aa1f8aec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Delete Product/DP002 - User want to delete product that has been uploaded</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
