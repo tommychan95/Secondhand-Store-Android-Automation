9e6b071ec836e147a6a99e45f89abfdcd2cf53a5
Tommy Chaniago <tommy.chaniago42@gmail.com>
2023-02-19 13:59:35 +0000

    add feature test suites ts

---
+++ Test Suites/Feature_Preview.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Preview</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d6ca884f-16f5-4e03-b94a-8d924cfe7e38</testSuiteGuid>
+   <testCaseLink>
+      <guid>fbcd9f54-d545-43cf-a997-e1bdd7e96eac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Preview/PRE001 - Seller want to preview product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7dec4985-25ed-41d3-befc-00aa9a81495b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Preview/PRE002 - Seller want to preview product while leave one of the field blank</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
